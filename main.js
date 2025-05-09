2d318f2036e945b3449fb05b0daf739f4ec738d5
lucassj10 <lucas.santanna.justino@gmail.com>
2025-05-08 23:12:42 -0300

    Add button to change grid size

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 
 
-const containerSize = 600
+const containerSize = 500
 let col = 18;
 let row = 18;
-let gridSize = row * col;
 
 const container  = document.querySelector(".container");
 const root = document.documentElement; //get de root pseudo-class
 root.style.setProperty(`--cellWidth`, `${(containerSize / col)}px`);
 root.style.setProperty(`--cellHeigth`, `${(containerSize / row)}px`);
 
-function createGrid(size){
-
-    for ( let i = 0; i < size; i++ ) {  
+function createGrid(){
+    let gridSize = row * col;
+    for ( let i = 0; i < gridSize; i++ ) {  
         const div = document.createElement("div");
         container.appendChild(div);
         div.addEventListener( "mouseover", ()=>{
@@ -23,6 +22,27 @@ function createGrid(size){
     
 }
 
+createGrid();
 
+function eraseGrid(){
+    while ( container.firstChild ){
+        container.removeChild(container.lastChild);
+    }
+}
 
-createGrid(gridSize);
+const gridBtn = document.querySelector("#change-grid")
+let newGrid;
+gridBtn.addEventListener( "click", ()=>{
+    newGrid = +prompt("Enter the new grid size (Max 100)");
+    if ( newGrid < 100 ){
+        col = newGrid;
+        row = newGrid;
+        root.style.setProperty(`--cellWidth`, `${(containerSize / col)}px`);
+        root.style.setProperty(`--cellHeigth`, `${(containerSize / row)}px`);
+        eraseGrid();
+        createGrid();
+    } else {
+        alert("Enter a number below 100");
+    }
+    
+} )
